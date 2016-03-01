cd44ec65c5027a580e4c7014fe04dd50e074f16e
myhirra <myhirra@126.com>
2016-03-01 15:57:37 +0800

    add js easy to make error

---
+++ basic/js_easy_to_make_error/index.js
@@ -0,0 +1,17 @@
+//test map & parseInt
+console.log(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'].map(parseInt));
+
+//test float calculate
+console.log(0.1 + 0.2 == 0.3);
+
+//test NaN
+console.log(NaN == NaN);
+
+// == & ===
+console.log(1 == '1', 1 === '1');
+
+// toString
+// console.log(1.toString());
+console.log(1 .toString());
+console.log(1.0 .toString());
+console.log(1 .toString());
